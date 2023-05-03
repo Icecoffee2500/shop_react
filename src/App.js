64c278ab5cae0655f963cc53178ade9811495600
QWEST <icecoffee2500@gmail.com>
2023-05-03 15:03:45 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import MyNav from './MyNav';
 import './App.css';
+import './font.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* HEADER */}
+      <MyNav />
+      {/* BODY */}
+      {/* <div className='main-bg' style={{ backgroundImage : 'url(./imgs/bg_nike.png)' }}></div> */}
+
+      <div className="flex" >
+        <Card className='card' style={{ width: '18rem' }}>
+          <Card.Img className='image' variant="top" src="./imgs/swoosh.png" />
+          <Card.Body>
+            <Card.Title className='main_font'>Nike</Card.Title>
+            <Card.Text className='second_font' >
+              Just Do It.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className='card' style={{ width: '18rem' }}>
+          <Card.Img className='image' variant="top" src="./imgs/tesla-logo-png.png" />
+          <Card.Body>
+            <Card.Title className='main_font'>Tesla</Card.Title>
+            <Card.Text className='second_font' >
+              Just Do It.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className='card' style={{ width: '18rem' }}>
+          <Card.Img className='image' variant="top" src="./imgs/apple-logo.png" />
+          <Card.Body>
+            <Card.Title className='main_font'>Apple</Card.Title>
+            <Card.Text className='second_font' >
+              Just Do It.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+
+
+      <Row>
+        <Col sm style={{ width: '18rem' }}>
+          <img className='mycard' src='./imgs/scott-jordan.png' />
+          <Col sm style={{height : "50px", marginBottom : "16px"}}>
+            <h4 className='second_font'>Air Jordan 1 Low</h4>
+            <h4 className='bold_font'>Travis Scott x Fragment</h4>
+          </Col>
+          <p className='second_font'>
+            It's the rare sneaker that satisfies the "rule of three". And no need for double-takes at the Sail Swoosh—it's indeed backwards—now a signature Scott touch for his Air Jordan 1 designs.
+          </p>
+        </Col>
+        <Col sm style={{ width: '18rem' }}>
+          <img className='mycard' src='./imgs/cybertruck.avif' />
+          <Col sm style={{height : "50px", marginBottom : "16px"}}>
+            {/* <img src='./imgs/Cybertrucklogo.png' style={{objectFit: "contain"}}/> */}
+            <h4 className='bold_font'>Cyber Truck</h4>
+          </Col>
+          <p className='second_font'>
+            Cybertruck is built with an exterior shell made for ultimate durability and passenger protection. Starting with a nearly impenetrable exoskeleton, every component is designed for superior strength and endurance, from Ultra-Hard 30X Cold-Rolled stainless-steel structural skin to Tesla armor glass.
+          </p>
+        </Col>
+        <Col sm style={{ width: '18rem' }}>
+          <img className='mycard' src='./imgs/apple-watch.png' />
+          <Col sm style={{height : "50px", marginBottom : "16px"}}>
+            <h4 className='bold_font'>Apple Watch</h4>
+          </Col>
+          <p className='second_font'>Apple Watch can do what your other devices can’t because it’s on your wrist. When you wear it, you get a fitness partner that measures all the ways you move, meaningful health insights, and a connection to the people and things you care about most.</p>
+        </Col>
+      </Row>
+      
     </div>
   );
 }
---
+++ src/MyNav.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './App.css';
+
+function MyNav() {
+    return (
+        <div className="Nav">
+            <Navbar collapseOnSelect expand="lg" bg="white" variant="black">
+            <Container>
+            <Navbar.Brand href="#home"><img className='img' src = '/imgs/magic_botton.png'/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                <Nav.Link href="#home">home</Nav.Link>
+                <Nav.Link href="#features">features</Nav.Link>
+                <Nav.Link href="#pricing">pricing</Nav.Link>
+                </Nav>
+                <Nav>
+                <Nav.Link href = "#mypage"> 임태헌 </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </div>
+    );
+}
+
+export default MyNav;
